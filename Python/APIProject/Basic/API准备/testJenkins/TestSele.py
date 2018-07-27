9875e65a544f5c5a8e6b26ffbd3acd2b508936a7
liuzhengxing <michaellzx.com>
2018-07-27 09:20:35 +0800

    修改了setup和teardown

--- Python/APIProject/Basic/API准备/testJenkins/TestSele.py
+++ Python/APIProject/Basic/API准备/testJenkins/TestSele.py
@@ -5,17 +5,18 @@ from APIProject.Basic.API准备.testJenkins.TestSele_public import *
 
 class TestUnitTest(unittest.TestCase):
     def setUp(self):
-        login()
-        # print("test setup")
+        # login()
+        print("test setup")
 
     def testOne(self):
         # createMaterial()
         print("testOne")
 
-    def testTwo(self):
+    @unittest.skip("skip with no reason")
+    def testTwo(self,ignore):
         print("testTwo")
 
     def tearDown(self):
-        logOut()
-        # print("test teardown")
+        # logOut()
+        print("test teardown")
 
