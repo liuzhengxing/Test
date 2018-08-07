bcd91750536de7989ac9c1e715c907290e936f06
liuzhengxing <michaellzx.com>
2018-08-07 17:13:40 +0800

    commit and pull together

---
+++ Python/APIProject/Basic/API准备/testProcess.py
@@ -0,0 +1,12 @@
+#encoding = utf-8
+
+# import time
+# for i in range(101):
+#     print("\r{:3}%".format(i),end="")
+#     time.sleep(0.1)
+
+guess = eval(input("input the data"))
+if guess == 99:
+    print("Yes")
+else:
+    print("NO")
